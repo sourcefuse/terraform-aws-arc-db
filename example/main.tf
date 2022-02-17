85fc3562b389caed8c441e5254673e614e4e2222
jamescrowley321 <jamescrowley151@gmail.com>
2022-02-16 20:47:10 -0600

    update example with working configuration:

--- example/main.tf
+++ example/main.tf
@@ -6,7 +6,6 @@ provider "aws" {
 module "ref_arch_db" {
   source              = "../."
   region              = var.region
-  security_groups     = concat(data.aws_security_groups.db_sg.ids, data.aws_security_groups.eks_sg.ids)
   allowed_cidr_blocks = [data.aws_vpc.vpc.cidr_block]
   subnets             = data.aws_subnet_ids.private.ids
   vpc_id              = data.aws_vpc.vpc.id

9ab4a5770c376247cfabba902b5c9c2be8ff21d5
Travis Saucier <travis.saucier@sourcefuse.com>
2022-08-13 10:59:43 -0400

    add new variables. formatting.

--- variables.tf
+++ variables.tf
@@ -14,14 +14,29 @@ variable "vpc_id" {
 ################################################################################
 ## aurora
 ################################################################################
+variable "aurora_cluster_enabled" {
+  type        = bool
+  description = "Enable creation of an Aurora Cluster"
+  default     = false
+}
+
 variable "aurora_cluster_name" {
   type        = string
   description = "Database name (default is not to create a database)"
+  default     = ""
 }
 
 variable "aurora_db_admin_username" {
   type        = string
   description = "Name of the default DB admin user role"
+  default     = ""
+}
+
+variable "aurora_db_admin_password" {
+  type        = string
+  description = "Password of the DB admin"
+  sensitive   = true
+  default     = ""
 }
 
 variable "aurora_db_name" {
@@ -81,12 +96,13 @@ variable "aurora_instance_type" {
 variable "aurora_subnets" {
   type        = list(string)
   description = "Subnets for the cluster to run in."
+  default     = []
 }
 
 variable "aurora_security_groups" {
   type        = list(string)
   default     = []
-  description = "List of security groups to be allowed to connect to the DB instance"
+  description = "List of security group IDs to be allowed to connect to the DB instance"
 }
 
 variable "aurora_allowed_cidr_blocks" {
@@ -149,7 +165,7 @@ variable "rds_instance_database_port" {
 
 variable "rds_instance_engine" {
   type        = string
-  description = "Database engine type. Required unless a snapshot_identifier or replicate_source_db is provided."
+  description = "Database engine type. Required unless a snapshot_identifier or replicate_source_db is provided. For supported values, see the Engine parameter in API action CreateDBInstance."
   default     = "postgres"
 }
 
@@ -171,6 +187,33 @@ variable "rds_instance_db_parameter_group" {
   default     = "postgres14"
 }
 
+variable "rds_instance_db_parameter" {
+  type = list(object({
+    apply_method = string
+    name         = string
+    value        = string
+  }))
+  description = "A list of DB parameters to apply. Note that parameters may differ from a DB family to another"
+  default     = []
+}
+
+variable "rds_instance_db_options" {
+  type = list(object({
+    db_security_group_memberships  = list(string)
+    option_name                    = string
+    port                           = number
+    version                        = string
+    vpc_security_group_memberships = list(string)
+
+    option_settings = list(object({
+      name  = string
+      value = string
+    }))
+  }))
+  description = "A list of DB options to apply with an option group. Depends on DB engine"
+  default     = []
+}
+
 variable "rds_instance_option_group_name" {
   type        = string
   description = "Name of the DB option group to associate"
@@ -190,103 +233,103 @@ variable "rds_instance_publicly_accessible" {
 }
 
 variable "rds_instance_multi_az" {
-  type = bool
+  type        = bool
   description = "Set to true if multi AZ deployment must be supported"
-  default = false
+  default     = false
 }
 
 variable "rds_instance_storage_type" {
-  type = string
+  type        = string
   description = "One of 'standard' (magnetic), 'gp2' (general purpose SSD), or 'io1' (provisioned IOPS SSD)"
-  default = "gp2"
+  default     = "gp2"
 }
 
 variable "rds_instance_instance_class" {
-  type = string
+  type        = string
   description = "Class of RDS instance"
-  default = "db.t2.medium"
+  default     = "db.t2.medium"
 }
 
 variable "rds_instance_allocated_storage" {
-  type = number
+  type        = number
   description = "The allocated storage in GBs. Required unless a snapshot_identifier or replicate_source_db is provided."
-  default = 20
+  default     = 20
 }
 
 variable "rds_instance_storage_encrypted" {
-  type = bool
+  type        = bool
   description = "Specifies whether the DB instance is encrypted. The default is false if not specified"
-  default = true
+  default     = true
 }
 
 variable "rds_instance_snapshot_identifier" {
-  type = string
+  type        = string
   description = "Snapshot identifier e.g: rds:production-2019-06-26-06-05. If specified, the module create cluster from the snapshot"
-  default = null
+  default     = null
 }
 
 variable "rds_instance_auto_minor_version_upgrade" {
-  type = bool
+  type        = bool
   description = "Allow automated minor version upgrade (e.g. from Postgres 9.5.3 to Postgres 9.5.4)"
-  default = true
+  default     = true
 }
 
 variable "rds_instance_allow_major_version_upgrade" {
-  type = bool
+  type        = bool
   description = "Allow major version upgrade"
-  default = false
+  default     = false
 }
 
 variable "rds_instance_apply_immediately" {
-  type = bool
+  type        = bool
   description = "Specifies whether any database modifications are applied immediately, or during the next maintenance window"
-  default = true
+  default     = true
 }
 
 variable "rds_instance_maintenance_window" {
-  type = string
+  type        = string
   description = "The window to perform maintenance in. Syntax: 'ddd:hh24:mi-ddd:hh24:mi' UTC"
-  default = "Mon:03:00-Mon:04:00"
+  default     = "Mon:03:00-Mon:04:00"
 }
 
 variable "rds_instance_skip_final_snapshot" {
-  type = bool
+  type        = bool
   description = "If true (default), no snapshot will be made before deleting DB"
-  default = true
+  default     = true
 }
 
 variable "rds_instance_copy_tags_to_snapshot" {
-  type = bool
+  type        = bool
   description = "Copy tags from DB to a snapshot"
-  default = true
+  default     = true
 }
 
 variable "rds_instance_backup_retention_period" {
-  type = number
+  type        = number
   description = "Backup retention period in days. Must be > 0 to enable backups"
-  default = 0
+  default     = 0
 }
 
 variable "rds_instance_backup_window" {
-  type = string
+  type        = string
   description = "When AWS can perform DB snapshots, can't overlap with maintenance window"
-  default = "22:00-03:00"
+  default     = "22:00-03:00"
 }
 
 variable "rds_instance_security_group_ids" {
-  type = list(string)
+  type        = list(string)
   description = "The IDs of the security groups from which to allow ingress traffic to the DB instance"
-  default = []
+  default     = []
 }
 
 variable "rds_instance_allowed_cidr_blocks" {
-  type = list(string)
+  type        = list(string)
   description = "The whitelisted CIDRs which to allow ingress traffic to the DB instance"
-  default = []
+  default     = []
 }
 
 variable "rds_instance_subnet_ids" {
-  type = list(string)
+  type        = list(string)
   description = "List of subnet IDs for the DB. DB instance will be created in the VPC associated with the DB subnet group provisioned using the subnet IDs. Specify one of subnet_ids, db_subnet_group_name or availability_zone"
-  default = []
+  default     = []
 }

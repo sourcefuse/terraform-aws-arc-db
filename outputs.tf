df421889dce18de3678789d9f578cbc25ca5c162
Travis Saucier <travis.saucier@sourcefuse.com>
2022-08-15 10:40:42 -0400

    update outputs. add additional output names.

--- outputs.tf
+++ outputs.tf
@@ -1,42 +1,42 @@
 ################################################################################
 ## aurora
 ################################################################################
-output "name" {
+output "aurora_name" {
   value       = try(module.aurora_cluster[0].database_name, null)
   description = "Database name"
 }
 
-output "master_username" {
+output "aurora_master_username" {
   value       = try(module.aurora_cluster[0].master_username, null)
   description = "Username for the master DB user"
 }
 
-output "cluster_identifier" {
+output "aurora_cluster_identifier" {
   value       = try(module.aurora_cluster[0].cluster_identifier, null)
   description = "Cluster Identifier"
 }
 
-output "arn" {
+output "aurora_arn" {
   value       = try(module.aurora_cluster[0].arn, null)
   description = "Amazon Resource Name (ARN) of cluster"
 }
 
-output "endpoint" {
+output "aurora_endpoint" {
   value       = try(module.aurora_cluster[0].endpoint, null)
   description = "The DNS address of the RDS instance"
 }
 
-output "reader_endpoint" {
+output "aurora_reader_endpoint" {
   value       = try(module.aurora_cluster[0].reader_endpoint, null)
   description = "A read-only endpoint for the Aurora cluster, automatically load-balanced across replicas"
 }
 
-output "master_host" {
+output "aurora_master_host" {
   value       = try(module.aurora_cluster[0].master_host, null)
   description = "DB Master hostname"
 }
 
-output "replicas_host" {
+output "aurora_replicas_host" {
   value       = try(module.aurora_cluster[0].replicas_host, null)
   description = "Replicas hostname"
 }
@@ -44,4 +44,27 @@ output "replicas_host" {
 ################################################################################
 ## rds
 ################################################################################
-// TODO - add rds outputs
+output "rds_instance_arn" {
+  value = try(module.rds_instance[0].instance_arn, null)
+  description = "The RDS Instance AWS ARN."
+}
+
+output "rds_instance_endpoint" {
+  value = try(module.rds_instance[0].instance_endpoint, null)
+  description = "The DNS address to the RDS Instance."
+}
+
+output "rds_instance_hostname" {
+  value = try(module.rds_instance[0].hostname, null)
+  description = "Hostname of the RDS Instance."
+}
+
+output "rds_instance_id" {
+  value = try(module.rds_instance[0].instance_id, null)
+  description = "The RDS Instance AWS ID."
+}
+
+output "rds_instance_resource_id" {
+  value = try(module.rds_instance[0].resource_id, null)
+  description = "The RDS Instance AWS resource ID."
+}

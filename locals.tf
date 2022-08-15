e3306b304841e18f060bbc5aa4edd82e839bee80
Travis Saucier <travis.saucier@sourcefuse.com>
2022-08-15 10:41:07 -0400

    update how locals are handled. when conditions are true / false.

--- locals.tf
+++ locals.tf
@@ -1,24 +1,58 @@
 locals {
-  // TODO - improve this better when "enabled" conditions are false
-  ssm_params = try([
+  ## aurora
+  aurora_ssm_params = var.aurora_cluster_enabled == true ? [
     {
-      name  = "/${var.namespace}/${var.environment}/primary_cluster/cluster_admin_db_password"
+      name  = "/${var.namespace}/${var.environment}/${var.aurora_cluster_name}/cluster_admin_db_password"
       value = random_password.aurora_db_admin_password[0].result
       type  = "SecureString"
     },
     {
-      name  = "/${var.namespace}/${var.environment}/primary_cluster/cluster_admin_db_username"
+      name  = "/${var.namespace}/${var.environment}/${var.aurora_cluster_name}/cluster_admin_db_username"
       value = var.aurora_db_admin_username
       type  = "SecureString"
     },
     {
-      name  = "/${var.namespace}/${var.environment}/primary_cluster/cluster_endpoint"
+      name  = "/${var.namespace}/${var.environment}/${var.aurora_cluster_name}/cluster_endpoint"
       value = module.aurora_cluster[0].endpoint
       type  = "SecureString"
     }
-  ], [])
+  ] : []
 
-  ssm_tags = {
-    Name = "${var.namespace}-${var.environment}-db-cluster-ssm-param"
-  }
+  aurora_ssm_tags = var.aurora_cluster_enabled == true ? {
+    AuroraName = "${var.namespace}-${var.environment}-db-cluster-ssm-param"
+  } : {}
+
+  ## rds
+  rds_instance_ssm_params = var.rds_instance_enabled == true ? [
+    {
+      name  = "/${var.namespace}/${var.environment}/${var.rds_instance_name}/admin_db_password"
+      value = random_password.rds_db_admin_password[0].result
+      type  = "SecureString"
+    },
+    {
+      name  = "/${var.namespace}/${var.environment}/${var.rds_instance_name}/admin_db_username"
+      value = var.rds_instance_database_user
+      type  = "SecureString"
+    },
+    {
+      name  = "/${var.namespace}/${var.environment}/${var.rds_instance_name}/endpoint"
+      value = module.rds_instance[0].instance_endpoint
+      type  = "SecureString"
+    }
+  ] : []
+
+  rds_instance_ssm_tags = var.rds_instance_enabled == true ? {
+    RDSName = "${var.namespace}-${var.environment}-rds-instance-ssm-param"
+  } : {}
+
+  ## concat locals
+  ssm_params = concat(
+    local.aurora_ssm_params,
+    local.rds_instance_ssm_params,
+  )
+
+  ssm_tags = merge(
+    local.aurora_ssm_tags,
+    local.rds_instance_ssm_tags,
+  )
 }

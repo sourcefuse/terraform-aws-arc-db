0f3f024e79216661c83853398c332ee19ae76dce
Travis Saucier <89415058+tsaucier-sf@users.noreply.github.com>
2023-07-01 12:23:40 -0400

    Revert "add custom name overrides for new resources" (#42)
    This reverts commit b85d2473e4917ba8d7a17c443ea49e98f702bc0e.

--- example/main.tf
+++ example/main.tf
@@ -14,6 +14,7 @@ terraform {
 
 provider "aws" {
   region  = var.region
+  profile = var.profile
 }
 
 ################################################################################
--- locals.tf
+++ locals.tf
@@ -3,55 +3,50 @@ locals {
   instance_kms_id = "arn:aws:kms:${var.region}:${var.account_id}:key/${try(aws_kms_key.aurora_cluster_kms_key[0].key_id, aws_kms_key.rds_db_kms_key[0].key_id)}"
   s3_kms_alias    = try(var.s3_kms_alias_override ? var.s3_kms_alias_override != "" : var.s3_kms_alias_override, "arn:aws:kms:${var.region}:${var.account_id}:alias/aws/s3")
 
-  ## s3
-  s3_db_management_bucket_name = var.s3_db_management_bucket_name != null ? var.s3_db_management_bucket_name : "${var.namespace}-${var.environment}-db-management"
-
   ## aurora
-  aurora_cluster_name = var.aurora_cluster_name != null ? var.aurora_cluster_name : "${var.namespace}-${var.environment}"
   aurora_ssm_params = var.aurora_cluster_enabled == true ? [
     {
-      name  = "/${var.namespace}/${var.environment}/${local.aurora_cluster_name}/cluster_admin_db_password"
+      name  = "/${var.namespace}/${var.environment}/${var.aurora_cluster_name}/cluster_admin_db_password"
       value = random_password.aurora_db_admin_password[0].result
       type  = "SecureString"
     },
     {
-      name  = "/${var.namespace}/${var.environment}/${local.aurora_cluster_name}/cluster_admin_db_username"
+      name  = "/${var.namespace}/${var.environment}/${var.aurora_cluster_name}/cluster_admin_db_username"
       value = var.aurora_db_admin_username
       type  = "SecureString"
     },
     {
-      name  = "/${var.namespace}/${var.environment}/${local.aurora_cluster_name}/cluster_endpoint"
+      name  = "/${var.namespace}/${var.environment}/${var.aurora_cluster_name}/cluster_endpoint"
       value = module.aurora_cluster[0].endpoint
       type  = "SecureString"
     }
   ] : []
 
   aurora_ssm_tags = var.aurora_cluster_enabled == true ? {
-    AuroraName = "${local.aurora_cluster_name}-ssm-param"
+    AuroraName = "${var.namespace}-${var.environment}-db-cluster-ssm-param"
   } : {}
 
   ## rds
-  rds_instance_name = var.rds_instance_name != null ? var.rds_instance_name : "${var.namespace}-${var.environment}-${var.rds_instance_engine}"
   rds_instance_ssm_params = var.rds_instance_enabled == true ? [
     {
-      name  = "/${var.namespace}/${var.environment}/${local.rds_instance_name}/admin_db_password"
+      name  = "/${var.namespace}/${var.environment}/${var.rds_instance_name}/admin_db_password"
       value = random_password.rds_db_admin_password[0].result
       type  = "SecureString"
     },
     {
-      name  = "/${var.namespace}/${var.environment}/${local.rds_instance_name}/admin_db_username"
+      name  = "/${var.namespace}/${var.environment}/${var.rds_instance_name}/admin_db_username"
       value = var.rds_instance_database_user
       type  = "SecureString"
     },
     {
-      name  = "/${var.namespace}/${var.environment}/${local.rds_instance_name}/endpoint"
+      name  = "/${var.namespace}/${var.environment}/${var.rds_instance_name}/endpoint"
       value = module.rds_instance[0].instance_endpoint
       type  = "SecureString"
     }
   ] : []
 
   rds_instance_ssm_tags = var.rds_instance_enabled == true ? {
-    RDSName = "${local.rds_instance_name}-ssm-param"
+    RDSName = "${var.namespace}-${var.environment}-rds-instance-ssm-param"
   } : {}
 
   ## option group

bad3576b72f11f55c5cd04d010340e35644a76fe
mayank0202 <mayank.sharma@sourcefuse.com>
2023-09-13 12:42:39 +0530

    updated node to 18

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.2"
+  required_version = "~> 1.3"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.0"
+      version = "~> 4.0"
     }
 
     null = {
